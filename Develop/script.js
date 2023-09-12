c79bbf3d808439f36d0dd72a3a5936ee93e01875
Rainy <142264832+MzRainy@users.noreply.github.com>
2023-09-11 23:42:38 -0500

    final code

--- Develop/script.js
+++ Develop/script.js
@@ -1,81 +1,83 @@
+var pwLength = 10;
+var pwArray = [];
+var  lowerCaseArray = ['a' ,'b' ,'c' ,'d' ,'e','f','g' ,'h' ,'i' , 'j','k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r','s' ,'t' ,'u' ,'v' ,'w' ,'x' ,'y' ,'z'];
+var  upperCaseArray = ['A' ,'B' ,'C' ,'D' ,'E' ,'F' ,'G' ,'H' ,'I' ,'J' ,'K' ,'L' ,'M' ,'N' ,'O' ,'P' ,'Q' ,'R' ,'S' ,'T' ,'U' ,'V' ,'W' ,'X' ,'Y' ,'Z'];
+var  numberArray = [ '0','1','2','3','4','5','6','7','8','9'];
+var  symbolArray = ['!', '@', '#', '$', '%', '^', '&', '*','(',')','_', '-', '+', '~','<', '>', '?','_'];
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 //    - Create alert for password length both to short and to long.
-var pwLength = parseInt(
-  prompt('How many characters would you like your password to contain?'),
-  10
- );
-
- if (pwLength < 8) {
-  alert('Password length must be at least 8 characters');
-  } else if (pwLength > 128) {
-    alert('Password cannot exceed 128 characters');
-   }
-
-function getPrompt(){
-
-}
-
 //create function for password length
-
-function createPassword() {
-var pwdChar = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+<>? ~`;:|ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-var passwordLength = length;
-
-var pwdChar = new Uint32Array(length);
-window.crypto.getRandomValues(pwd);
-
-for (i=0; i<length; i++){
-  password += pwdChar[array[i] % pwdChar.length];
-}
-return password
-console.log(password)
-
-
-}
-
-createPassword(9)
-
-
 //create function for password choices
 //  - Create function for Lowercase 
 //  - Create function for Uppercase 
 //  - Create function for number 
 //  - Create function for special character
-
 //create user prompts
 //  - Ask password length
-
-
 //  - Ask password lowercase
-var pwLower = parseInt(
-  prompt('Do you want lowercase letters?'),
-  10
- );
-
- if (pwLength < 8) {
-  alert('Password length must be at least 8 characters');
-  } else if (pwLength > 128) {
-    alert('Password cannot exceed 128 characters');
-   }
 //  - Ask password uppercase
 //  - Ask password number
 //  - Ask password special character
-
 //create the randomization of password
 //  - Create the for loop
+//function 
+//if(pwLowerCase === false){
+// alert('Please select at least 1 true option.');
 
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+  
+// Write password to the #password input
+function writePassword() {
+  var finalPw = pwPrompts();
+  var passwordText = document.querySelector("#password");
+
+  if(finalPw){
+  var newPwd = generatePassword();  
+  passwordText.value = newPwd;
+  }
+}
+
+function generatePassword() {
+  var password = '';
+
+  for(var i = 0; i < pwLength; i++) {
+    var randLetter = math.floor(math.random() * pwArray.length)
+    password = password + pwArray[randLetter];
+  }
+return password;
+}
+
+function pwPrompts(){
+  pwArray = [];
+ 
+  pwLength = parseInt(prompt('How many characters would you like your password to contain? Please choose between 8 and 128.'));
+
+   if (isNaN(pwLength) || pwLength < 8 || pwLength >128) {
+    alert('Password length must be between 8- 128. Please answer with a digit.');
+   return false;
+   }
+    console.log(pwLength);
+
+    if (confirm('Do you want an uppercase letter?'));
+    pwArray = pwArray.concat(lowerCaseArray);
+    console.log(lowerCaseArray);
+
+   if  (confirm('Do you want an uppercase letter?'));
+   pwArray = pwArray.concat(upperCaseArray);
+   console.log(upperCaseArray);
+  
+    if (confirm('Do you want numbers?'));
+    pwArray = pwArray.concat(numberArray);
+    console.log(numberArray);
+  
+    if (confirm('Do you want special characters?'));
+    pwArray = pwArray.concat(symbolArray);
+    console.log(symbolArray);
+  
+  }
+  return true;
+
+
+
